f2269beefb1aba1149d99ceffcb1611ef38ba4e8
Natalia Podorozhnaya <arkadiy.dryzhak@yandex.ru>
2022-09-18 17:23:52 +0300

    Added soluton to problem 1

---
+++ task.js
@@ -0,0 +1,88 @@
+class PrintEditionItem{
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    set state(newState) {
+        if (newState < 0) {
+            this._state = 0;
+        } else if (newState > 100) {
+            this._state = 100;
+        } else {
+            this._state = newState;
+        }
+    }
+
+    get state(){
+        return this._state;
+    }
+
+    fix() {
+        return (this.state < 100)? (this.state * 1.5) : this.state;
+    }
+    
+}
+
+const sherlock = new PrintEditionItem(
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+  );
+  
+  console.log(sherlock.releaseDate); //2019
+  console.log(sherlock.state); //100
+  sherlock.fix();
+  console.log(sherlock.state); //100
+
+class Magazine extends PrintEditionItem{
+    constructor(name, releaseDate, pagesCount){
+        super (name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem{
+    constructor(author, name, releaseDate, pagesCount){
+        super (name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "book";
+    }
+}
+
+class NovelBook extends Book{
+    constructor(author, name, releaseDate, pagesCount){
+        super (author, name, releaseDate, pagesCount);
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book{
+    constructor(author, name, releaseDate, pagesCount){
+        super (author, name, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book{
+    constructor(author, name, releaseDate, pagesCount){
+        super (author, name, releaseDate, pagesCount);
+        this.type = "detective";
+    }
+}
+
+const picknick = new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+  );
+  
+  console.log(picknick.author); //"Аркадий и Борис Стругацкие"
+  picknick.state = 10;
+  console.log(picknick.state); //10
+  picknick.fix();
+  console.log(picknick.state); //15
